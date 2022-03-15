4ba6dfad56ed53c86986173ded491d7870d008b8
Yancy Matherne <github@yancyjr.com>
2022-03-15 15:15:29 -0500

    Only handle keys while the game is still playing.
    - Before, the letters would still be accepted into the currentGuess even though
    the board doesn't display it. The user could submit a guess triggering the
    invalid word check even though the game state itself is not changing.

--- src/lib/keyboard/keyboardActions.ts
+++ src/lib/keyboard/keyboardActions.ts
@@ -1,11 +1,14 @@
-import { gameState, submitGuess } from '../../stores';
+import { get } from 'svelte/store';
+import { gameState, submitGuess, isStillPlaying } from '../../stores';
 
 export const handleKey = (key: string) => {
-	if (key.length === 1 && 'a' <= key && key <= 'z') {
-		gameState.addLetter(key);
-	} else if (key === 'enter') {
-		submitGuess();
-	} else if (key === 'backspace' || key === 'delete') {
-		gameState.removeLetter();
+	if (isStillPlaying(get(gameState))) {
+		if (key.length === 1 && 'a' <= key && key <= 'z') {
+			gameState.addLetter(key);
+		} else if (key === 'enter') {
+			submitGuess();
+		} else if (key === 'backspace' || key === 'delete') {
+			gameState.removeLetter();
+		}
 	}
 };
