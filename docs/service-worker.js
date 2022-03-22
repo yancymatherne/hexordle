245bb47c9f937df9fe919f4e87fd04c824e2453c
Yancy Matherne <ymatherne@boundlessgeo.com>
2022-03-22 01:12:12 -0500

    Build new version of app.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,18 +1,18 @@
-const timestamp = 1647548114453;
+const timestamp = 1647929399593;
 const build = [
-  "/hexordle/internal/start-a081f2a8.js",
-  "/hexordle/internal/pages/__layout.svelte-ec2cd56a.js",
-  "/hexordle/internal/assets/pages/__layout.svelte-2cd3303a.css",
-  "/hexordle/internal/error.svelte-168f833c.js",
-  "/hexordle/internal/pages/index.svelte-5f3e15be.js",
-  "/hexordle/internal/assets/pages/index.svelte-5ef8e306.css",
-  "/hexordle/internal/pages/settings/index.svelte-776cb792.js",
-  "/hexordle/internal/pages/stats/index.svelte-12562cd5.js",
+  "/hexordle/internal/start-4dbd9fd2.js",
+  "/hexordle/internal/pages/__layout.svelte-1cf19f55.js",
+  "/hexordle/internal/assets/pages/__layout.svelte-5291c7e2.css",
+  "/hexordle/internal/error.svelte-f093786e.js",
+  "/hexordle/internal/pages/index.svelte-61070f5b.js",
+  "/hexordle/internal/assets/pages/index.svelte-5f52148b.css",
+  "/hexordle/internal/pages/settings/index.svelte-397856fd.js",
+  "/hexordle/internal/pages/stats/index.svelte-5fbaf473.js",
   "/hexordle/internal/assets/pages/stats/index.svelte-ad656fcf.css",
-  "/hexordle/internal/chunks/vendor-b66e214f.js",
+  "/hexordle/internal/chunks/vendor-ddc51e67.js",
   "/hexordle/internal/chunks/paths-4b3c6e7e.js",
-  "/hexordle/internal/chunks/stores-3cd064d2.js",
-  "/hexordle/internal/chunks/Guess-bb209419.js",
+  "/hexordle/internal/chunks/stores-4e90ffbc.js",
+  "/hexordle/internal/chunks/Guess-ae42788d.js",
   "/hexordle/internal/assets/Guess-8f79f69d.css"
 ];
 const files = [
